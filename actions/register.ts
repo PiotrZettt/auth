1a014c9993b6c7930c06635ed5c82e80e656b0e3
PiotrZettt <piotrzielinski@o2.pl>
2025-03-22 17:34:11 +0000

    Initial commit

--- actions/register.ts
+++ actions/register.ts
@@ -5,7 +5,6 @@ import * as z from "zod"
 
 export const register = async (values: z.infer<typeof RegisterSchema>) => {
     const validatedFields = RegisterSchema.safeParse(values)
-    console.log(validatedFields)
     if (!validatedFields.success) {
         return {error: "Invalid fields!"}
     }
